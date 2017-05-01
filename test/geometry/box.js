4b8187e9da3e850e415981ac85382d81b299073a
vipyne <vipyne@gmail.com>
2017-05-01 15:34:13 -0400

    box geometry tests

--- test/geometry/box.js
+++ test/geometry/box.js
@@ -26,29 +26,65 @@ test('new BoxGeometry(opts) -> Function', ({
   pass,
   end
 }) => {
-  plan(3)
+  plan(14)
 
   if ('function' == typeof BoxGeometry) {
     pass('is function.')
   }
 
-  // throws(() => { createBoxGeometry({segments: 'foo'}) },
-  //       TypeError,
-  //       'throws TypeError when segments is not a number.')
+  throws(() => { createBoxGeometry({x: 'foo'}) },
+        TypeError,
+        'throws TypeError when `x` is not a number.')
+
+  throws(() => { createBoxGeometry({y: 'foo'}) },
+        TypeError,
+        'throws TypeError when `y` is not a number.')
+
+  throws(() => { createBoxGeometry({z: {}}) },
+        TypeError,
+        'throws TypeError when `z` is not a number.')
+
+
+  throws(() => { createBoxGeometry({segments: 'foo'}) },
+        TypeError,
+        'throws TypeError when segments is not a number.')
+
+
+  throws(() => { createBoxGeometry({segments: {x:'x'}}) },
+        TypeError,
+        'throws TypeError when segments.x is not a number.')
+
+  throws(() => { createBoxGeometry({segments: {x: 4, y:'y'}}) },
+        TypeError,
+        'throws TypeError when segments.y is not a number.')
+
+  throws(() => { createBoxGeometry({segments: {z:'z'}}) },
+        TypeError,
+        'throws TypeError when segments.z is not a number.')
 
   const nullBox = createBoxGeometry(null)
   deepEqual({
           flatten: false,
           size: { x: 1, y: 1, z: 1 },
           segments: { x: 1, y: 1, z: 1 }
-        }, nullBox, 'creates default Box if null is passed in as argument')
+        }, nullBox, 'creates default Box if null is passed in as argument.')
 
   const anotherBox = createBoxGeometry({x: 3})
   deepEqual({
           flatten: false,
           size: { x: 3, y: 1, z: 1 },
           segments: { x: 1, y: 1, z: 1 }
-        }, anotherBox, 'creates default Box if incomplete obj is passed in as argument')
+        }, anotherBox, 'creates default Box if incomplete opts object is passed in as argument.')
+
+  const superBox = createBoxGeometry({x: 3})
+  assert(undefined != superBox.complex)
+  assert(null != superBox.complex)
+
+  const sizeBox = createBoxGeometry({x: 3})
+  deepEqual({ x: 3, y: 1, z: 1 }, sizeBox.size, 'assigns size to {x,y,z} object.')
+
+  const segBox = createBoxGeometry({x: 3})
+  deepEqual({ x: 1, y: 1, z: 1 }, segBox.segments, 'assigns segments object.')
 
   end()
 })
