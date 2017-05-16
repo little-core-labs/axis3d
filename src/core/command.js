72578df2dcd5a09615e070650e51c4315ea43d0f
vipyne <vipyne@gmail.com>
2017-05-16 10:46:43 -0400

    what is this??!?! not just a faith no more quote

--- src/core/command.js
+++ src/core/command.js
@@ -81,10 +81,11 @@ export class Command extends Function {
     super(Command.codegen(function wrap(fn) {
       return fn.apply(fn, Array.prototype.slice.call(arguments, 1))
     }))
+    let self = this
     const exec = (...args) => this(fn, ...args)
     const id = Command.id()
-    return Object.assign((...args) => exec.call(this, ...args), {
-      this, id
+    return Object.assign((...args) => exec.call(self, ...args), {
+      self, id
     })
   }
 }
