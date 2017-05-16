bfb47d5864368425a10b0d8994ce90621ad74d33
vipyne <vipyne@gmail.com>
2017-05-16 12:36:09 -0400

    essentially where I was last week but in 1/25th the time

--- example/audio-vis/index.js
+++ example/audio-vis/index.js
@@ -4,9 +4,8 @@ import {
   PerspectiveCamera,
   DirectionalLight,
   SphereGeometry,
-  PhongMaterial,
   Quaternion,
-  Texture,
+  Material,
   Context,
   Frame,
   Color,
@@ -15,14 +14,63 @@ import {
 
 import quat from 'gl-quat'
 
+///////////// Web Audio Api /////////////
+const audioCtx = new AudioContext()
+const analyser = audioCtx.createAnalyser()
+const sourceBuffer = audioCtx.createBufferSource()
+const jsScriptNode = audioCtx.createScriptProcessor(512, 1, 1)
+const bufferLength = analyser.frequencyBinCount
+const dataArray = new Uint8Array(bufferLength)
+
+analyser.smoothingTimeConstant = 0.1
+analyser.fftSize = 512
+
+sourceBuffer.connect(analyser)
+sourceBuffer.connect(audioCtx.destination)
+
+jsScriptNode.connect(audioCtx.destination)
+
+const request = new XMLHttpRequest()
+request.open('GET', 'assets/audio.mp3', true)
+request.responseType = 'arraybuffer'
+request.onload = () => {
+
+  audioCtx.decodeAudioData(request.response, (buffer) => {
+    sourceBuffer.buffer = buffer
+
+    jsScriptNode.onaudioprocess = (e) => {
+      // fill array with frequency data
+      analyser.getByteFrequencyData(dataArray)
+    }
+
+    // play audio
+    sourceBuffer.start(audioCtx.currentTime)
+  })
+}
+request.send()
+///////////// Web Audio Api /////////////
+
+
 const ctx = Context()
 
 const camera = PerspectiveCamera(ctx)
 const sphere = Mesh(ctx, { geometry: SphereGeometry() })
 const frame = Frame(ctx)
 const light = DirectionalLight(ctx)
-const texture = Texture(ctx)
-const material = PhongMaterial(ctx, {map: texture})
+const material = Material(ctx, {
+  uniforms: {
+    dArray(reglCtx, opts) { return opts.dArray }
+  },
+  fragmentShaderMain:
+  `
+  uniform float dArray;
+  void main() {
+    GeometryContext geometry = getGeometryContext();
+
+    gl_FragColor = vec4(dArray/256.0,0.3,0.5,1.0);
+  }
+  `
+})
 const rotation = Quaternion()
 
 const image = new Image()
@@ -37,8 +85,9 @@ frame(({time, cancel}) => {
   quat.slerp(rotation, rotation, multiply(multiply(x, y), z), 0.5)
   camera({rotation, position: [0, 0, 5]}, () => {
     light({intensity: 0.8, position: [0, 0, 10], ambient: 0.01})
-    texture({data: image})
-    material({specular: Color('dim gray') }, () => {
+    material({
+      dArray: dataArray[100]
+    }, () => {
       sphere({})
     })
   })
