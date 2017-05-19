6af194c943262955485fc0d0f4a872d104213288
vipyne <vipyne@gmail.com>
2017-05-19 11:33:15 -0400

    cleanup

--- example/audio-vis/index.js
+++ example/audio-vis/index.js
@@ -26,7 +26,7 @@ import {
 
 import quat from 'gl-quat'
 
-///////////// Web Audio Api /////////////
+///////////// start Web Audio API code /////////////
 const audioCtx = new AudioContext()
 const analyser = audioCtx.createAnalyser()
 const sourceBuffer = audioCtx.createBufferSource()
@@ -56,9 +56,8 @@ request.onload = () => {
     jsScriptNode.onaudioprocess = (e) => {
       // fill array with frequency data
       analyser.getByteFrequencyData(frequencyDataArray)
+      // fill array with frequency data
       analyser.getByteTimeDomainData(waveformDataArray)
-      // console.log(frequencyDataArray.length / 64)
-      // console.log(analyser.frequencyBinCount / 8)
     }
 
     // play audio
@@ -66,13 +65,15 @@ request.onload = () => {
   })
 }
 request.send()
-///////////// Web Audio Api /////////////
+///////////// end Web Audio API code /////////////
 
+///////////// start Axis3D code /////////////
 const ctx = Context()
 
 const camera = PerspectiveCamera(ctx)
 const frame = Frame(ctx)
 const rotation = Quaternion()
+const fDataTexture = Texture(ctx)
 
 // inputs
 const orientation = OrientationInput(ctx)
@@ -86,36 +87,37 @@ const orbitCamera = OrbitCameraController(ctx, {
   camera, inputs,
   invert: true,
   interpolationFactor: 0.1,
-  // euler: [0, 0.5*Math.PI, 0]
+  euler: [0.5*Math.PI, 0, 0]
 })
 
-const dataTexture = Texture(ctx)
 
-let geo = PlaneGeometry({segments: {x: 256, y: 1}, size: {x: 10, y: 1}})
+let geometry = PlaneGeometry({ segments: {x: 256, y: 1},
+                               size: {x: 10, y: 1}
+                            })
 
-const sphere = Mesh(ctx, {
-  geometry: geo,
+const plane = Mesh(ctx, {
+  geometry: geometry,
   uniforms: Object.assign(new MeshUniforms(ctx), {
-    dataTexture(reglCtx, o) {
-      return reglCtx.texture(o.dataTextureArgs)
+    fDataTexture(reglCtx, opts) {
+      return reglCtx.texture(opts.fDataTextureArgs)
     },
-    dataTexture2(reglCtx, o) {
-      return reglCtx.texture(o.dataTextureArgs2)
+    wDataTexture(reglCtx, opts) {
+      return reglCtx.texture(opts.wDataTextureArgs)
     }
   }),
   vertexShaderTransform:
   `
-  uniform sampler2D dataTexture;
-  uniform sampler2D dataTexture2;
+  uniform sampler2D fDataTexture;
+  uniform sampler2D wDataTexture;
 
   float lerp(float x, float y, float a) {
     return x * (1.0 - a) + y * a;
   }
   void transform () {
-    float offsetX = texture2D(dataTexture, uv).x;
+    float offsetX = texture2D(fDataTexture, uv).x;
     offsetX = lerp(gl_Position.x, gl_Position.x + (offsetX/1.0) + gl_Position.x, 0.701);
 
-    float offsetY = texture2D(dataTexture2, uv).x;
+    float offsetY = texture2D(wDataTexture, uv).x;
     offsetY = lerp(gl_Position.y, gl_Position.y + (offsetY/1.0) + gl_Position.y, 0.701);
 
     gl_Position = vec4(offsetX, offsetY, gl_Position.zw);
@@ -124,55 +126,51 @@ const sphere = Mesh(ctx, {
 })
 
 const material = Material(ctx, {
-  // texture needs to be defined before uniforms
-  map: dataTexture,
+  map: fDataTexture,
   uniforms: {
-    dataTexture(reglCtx, o) {
-      return reglCtx.texture(o.dataTexture)
+    fDataTexture(reglCtx, opts) {
+      return reglCtx.texture(opts.dataTexture)
     },
-    dArray(reglCtx, opts) {
-      return opts.dArray
+    singleDataPoint(reglCtx, opts) {
+      return opts.singleDataPoint
     },
   },
   fragmentShaderMain:
   `
-  uniform float dArray;
-  uniform sampler2D dataTexture;
+  uniform float singleDataPoint;
+  uniform sampler2D fDataTexture;
   void main() {
     GeometryContext geometry = getGeometryContext();
 
-    float blue = texture2D(dataTexture, geometry.uv).b;
-    float green = texture2D(dataTexture, geometry.uv).g;
-    float red = texture2D(dataTexture, geometry.uv).r;
+    float green = texture2D(fDataTexture, geometry.uv).g;
 
-    // gl_FragColor = vec4(red, green, blue, 1.0);
-    gl_FragColor = vec4(dArray/256.0, green, 0.5, 1.0);
+    gl_FragColor = vec4(singleDataPoint/256.0, green, 0.5, 1.0);
   }
   `
 })
 const angle = Quaternion()
 
 frame(({time, cancel}) => {
-  orbitCamera({ position: [0, 0, 3], target: [0, 0, 0] }, () => {
+  orbitCamera({ position: [0, 0, 5], target: [0, 0, 0] }, () => {
     quat.setAxisAngle(angle, [0, 1, 0], 0.25)
     material({
       cull: false,
-      dArray: frequencyDataArray[100],
-      dataTexture: {data:  frequencyDataArray,
-                         width: 256,
-                         height: 1
-                  },
+      singleDataPoint: frequencyDataArray[100],
+      fDataTexture: {data: frequencyDataArray,
+                           width: 256,
+                           height: 1
+                    },
     }, () => {
-      sphere([{
-        dataTextureArgs: {data:  frequencyDataArray,
-                         width: 256,
-                         height: 1
-                  },
-        dataTextureArgs2: {data:  frequencyDataArray,
-                         width: 256,
-                         height: 1
-                  }
-      }, {}])
+      plane([{
+        fDataTextureArgs: {data: frequencyDataArray,
+                                 width: 256,
+                                 height: 1
+                          },
+        wDataTextureArgs: {data: waveformDataArray,
+                                 width: 256,
+                                 height: 1
+                          }
+      }, {  }])
     })
   })
 })
