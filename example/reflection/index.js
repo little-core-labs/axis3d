e91629732fb1b9358bd147a51057a8410cba2247
vipyne <vipyne@gmail.com>
2017-05-22 16:31:41 -0400

    reflection example

--- example/reflection/index.js
+++ example/reflection/index.js
@@ -5,6 +5,7 @@ import {
 } from '../../extras/controller'
 
 import {
+  ReflectionMaterial,
   PerspectiveCamera,
   OrientationInput,
   MaterialUniforms,
@@ -52,71 +53,31 @@ const orbitCamera = OrbitCameraController(ctx, {
   interpolationFactor: 0.1,
 })
 
-let i = 0
-const reflectMaterial = Material(ctx, {
-  map: texture,
-  uniforms: {
-    envmap(reglCtx, opts) {
-      return !i++ ? reglCtx.texture(reglCtx.textureData) : reglCtx.texture
-    },
-    invertedView(reglCtx, opts) {
-      return mat4.invert([], reglCtx.view)
-    }
-  },
-  fragmentShaderMain:
-  `
-  uniform sampler2D envmap;
-  varying vec3 vReflectDir;
-
-  vec4 lookupEnv(vec3 dir) {
-    float PI = 3.14;
-    float lat = atan(dir.z, dir.x);
-    float lon = acos(dir.y / length(dir));
-    vec2 envLoc = vec2(0.5 + lat / (2.0 * PI), lon / PI);
-
-    return texture2D(envmap, envLoc);
-  }
-
-  void main() {
-    GeometryContext geometry = getGeometryContext();
-
-    gl_FragColor = lookupEnv(vReflectDir);
-  }
-  `
+const reflectiveMaterial = ReflectionMaterial(ctx, {
+  envmap: texture,
 })
 
 const bunny = Mesh(ctx, {
   geometry: stanfordBunny,
-  uniforms: Object.assign(new MeshUniforms(ctx), {
-    invertedView(reglCtx, opts) {
-      return mat4.invert([], reglCtx.view)
-    }
-  }),
-  vertexShaderTransform:
-  `
-  varying vec3 vReflectDir;
-  uniform mat4 invertedView;
-
-  void transform() {
-    vec3 iv = invertedView[3].xyz / invertedView[3].w;
-    vec3 eye = normalize(gl_Position.xyz - iv);
-
-    vReflectDir = reflect(eye, normal);
-  }
-  `
+  reflective: true,
 })
 
-const bgMaterial = FlatMaterial(ctx, {map: texture})
-const background = Mesh(ctx, { geometry: SphereGeometry(ctx)})
+const bgMaterial = FlatMaterial(ctx, {
+  map: texture
+})
+
+const background = Mesh(ctx, {
+  geometry: SphereGeometry(ctx)
+})
 
 frame(() => {
   orbitCamera({position: [-0.2,0,0], target: [0,0,0]}, () => {
     bgMaterial({cull: false}, () => {
       background({scale: [1, -1, 1] }, () => {
-      })
-        reflectMaterial({cull: false} , () => {
-          bunny({scale: 0.1, position: [0, -0.002, 0]})
+        reflectiveMaterial({} , () => {
+          bunny({scale: 0.1, position: [0, -0.02, 0]})
         })
+      })
     })
   })
 })
