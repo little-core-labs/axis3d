4971fbbc4647d989063fa142ae72837bda49b87e
Joseph Werle <joseph.werle@gmail.com>
2017-07-31 21:06:14 -0400

    wip

--- src/core/glsl/mesh/index.js
+++ src/core/glsl/mesh/index.js
@@ -5,6 +5,7 @@ Object.assign(exports, {
   [__dirname + '/uniforms']: glslify(__dirname + '/uniforms.glsl'),
   [__dirname + '/fragment']: glslify(__dirname + '/fragment.glsl'),
   [__dirname + '/vertex']: glslify(__dirname + '/vertex.glsl'),
+  [__dirname + '/main']: glslify(__dirname + '/main.glsl'),
   [__dirname + '/mesh']: glslify(__dirname + '/mesh.glsl'),
 })
 
--- src/core/mesh.js
+++ src/core/mesh.js
@@ -95,31 +95,7 @@ export class MeshShader extends Shader {
     super(ctx, {
       vertexShader: ({vertexShader}) => vertexShader || `
       #define GLSL_MESH_UNIFORM_VARIABLE ${uniformName}
-      #include <camera/camera>
-      #include <mesh/vertex>
-      #include <mesh/mesh>
-
-      #include <camera/uniforms>
-      #include <mesh/uniforms>
-
-      #include <vertex/attributes/position>
-      #include <vertex/attributes/normal>
-      #include <vertex/attributes/uv>
-
-      #include <varying/position>
-      #include <varying/normal>
-      #include <varying/uv>
-      #include <varying/emit>
-
-      #include <vertex/main>
-      void Main(inout vec4 vertexPosition, inout VaryingData data) {
-        vertexPosition = MeshVertex(
-          camera.projection,
-          camera.view,
-          ${uniformName}.model,
-          position);
-      }
-
+      #include <mesh/main>
      `,
 
       ...initialState
