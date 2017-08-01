afae28e2c8ef79f43d71463ec84dd0a25afd99a4
Joseph Werle <joseph.werle@gmail.com>
2017-07-31 21:30:45 -0400

    wip

--- src/core/glsl/mesh/main.glsl
+++ src/core/glsl/mesh/main.glsl
@@ -1,29 +1,30 @@
 #ifndef GLSL_MESH_MAIN
 #define GLSL_MESH_MAIN
 
-#include <camera/camera>
-#include <mesh/vertex>
-#include <mesh/mesh>
+#include "./variables"
+#include "./uniforms"
+#include "./vertex"
+#include "./mesh"
 
-#include <camera/uniforms>
-#include <mesh/uniforms>
+#include "../camera/camera"
+#include "../camera/uniforms"
 
-#include <vertex/attributes/position>
-#include <vertex/attributes/normal>
-#include <vertex/attributes/uv>
+#include "../vertex/attributes/position"
+#include "../vertex/attributes/normal"
+#include "../vertex/attributes/uv"
 
-#include <varying/position>
-#include <varying/normal>
-#include <varying/uv>
-#include <varying/emit>
+#include "../varying/position"
+#include "../varying/normal"
+#include "../varying/uv"
+#include "../varying/emit"
 
-#include <vertex/main>
+#include "../vertex/main"
 
 void Main(inout vec4 vertexPosition, inout VaryingData data) {
   vertexPosition = MeshVertex(
       camera.projection,
       camera.view,
-      ${uniformName}.model,
+      GLSL_MESH_UNIFORM_VARIABLE.model,
       position);
 }
 
