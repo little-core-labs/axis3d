967f3ada9c01791df2b6058a518c7ce2b6dbd1fd
Joseph Werle <joseph.werle@gmail.com>
2017-08-14 09:09:13 -0400

    refactor(src/frame/uniforms.js): Provide frame resolution uniform

--- src/frame/uniforms.js
+++ src/frame/uniforms.js
@@ -7,8 +7,10 @@ export class FrameShaderUniforms extends Component {
   static defaults() { return { ...defaults } }
   constructor(ctx, initialState = {}) {
     assignDefaults(initialState, FrameShaderUniforms.defaults())
+    const {uniformName} = initialState
     super(ctx, initialState,
-      new ShaderUniforms(ctx, {
+      new ShaderUniforms(ctx, {prefix: `${uniformName}.`}, {
+        resolution: ({drawingBufferWidth: w, drawingBufferHeight: h}) => [w, h],
         time: ({time}) => time,
         tick: ({tick}) => tick,
       })
