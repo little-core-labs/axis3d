a2bf11f635bd7db1125056522aaa622a625c6ba6
Joseph Werle <joseph.werle@gmail.com>
2017-08-22 17:22:35 -0400

    refactor(src/mesh/shader/defines): Add safer check

--- src/mesh/shader/defines.js
+++ src/mesh/shader/defines.js
@@ -10,17 +10,17 @@ export class MeshShaderDefines extends Component {
     super(ctx, initialState,
       new ShaderDefines(ctx, {
         GLSL_MESH_HAS_POSITION({geometry}) {
-          if (geometry.positions) { return true }
+          if (geometry && geometry.positions) { return true }
           return null
         },
 
         GLSL_MESH_HAS_NORMAL({geometry}) {
-          if (geometry.normals) { return true }
+          if (geometry && geometry.normals) { return true }
           return null
         },
 
         GLSL_MESH_HAS_UV({geometry}) {
-          if (geometry.uvs) { return true }
+          if (geometry && geometry.uvs) { return true }
           return null
         }
       })
