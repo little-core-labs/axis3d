1af01c01cbb7f6d4bc51f30e2572ee4a773bf591
Joseph Werle <joseph.werle@gmail.com>
2017-08-22 14:25:06 -0400

    refactor(texture/): Remove uniforms component

--- src/texture/2d/texture.js
+++ src/texture/2d/texture.js
@@ -1,4 +1,3 @@
-import { TextureShaderUniforms } from './uniforms'
 import { TextureContext } from './context'
 import { assignDefaults } from '../../utils'
 import * as defaults from './defaults'
@@ -6,10 +5,9 @@ import { Component } from '../../core'
 
 export class Texture extends Component {
   static defaults() { return { ...defaults } }
-  constructor(ctx, initialState = {}, ...children) {
+  constructor(ctx, initialState = {}) {
     assignDefaults(initialState, Texture.defaults())
     super(ctx, initialState,
-      new TextureContext(ctx, initialState),
-      new TextureShaderUniforms(ctx, initialState))
+      new TextureContext(ctx, initialState))
   }
 }
