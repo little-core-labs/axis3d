46c2b3efe05444efda52c2d9a710f58cff44fc75
Joseph Werle <joseph.werle@gmail.com>
2017-08-29 18:42:34 -0400

    feat(src/core/glsl/mesh/fragment): Add mesh fragment shader GLSL

--- src/core/glsl/mesh/index.js
+++ src/core/glsl/mesh/index.js
@@ -1,6 +1,7 @@
 import glslify from 'glslify'
 
 Object.assign(exports, {
+  [__dirname + '/fragment/main']: glslify(__dirname + '/fragment/main.glsl'),
   [__dirname + '/vertex/main']: glslify(__dirname + '/vertex/main.glsl'),
   [__dirname + '/variables']: glslify(__dirname + '/variables.glsl'),
   [__dirname + '/uniforms']: glslify(__dirname + '/uniforms.glsl'),
