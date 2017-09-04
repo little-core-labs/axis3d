be5bac9fa76e8e27fa1c88c5c0ba360776bc2d9d
Joseph Werle <joseph.werle@gmail.com>
2017-09-03 20:21:42 -0400

    refactor(src/object3d): Optimize compute with cached values

--- src/object3d/context/transform.js
+++ src/object3d/context/transform.js
@@ -8,17 +8,52 @@ export class Object3DTransformContext extends Component {
   static defaults() { return { ...defaults } }
   constructor(ctx, initialState = {}) {
     assignDefaults(initialState, Object3DTransformContext.defaults())
+    const matrix = mat4.identity(new Float32Array(16))
+    const previousTransform = []
+    const previousParent = []
+    const previousLocal = []
     super(ctx, initialState, new ScopedContext(ctx, {
       transform(ctx, args) {
         const {matrix: local, transform: parent} = ctx
-        const matrix = mat4.identity([])
         const {transform} = (args || {})
         // M' = Mp * M
-        if (parent) { mat4.multiply(matrix, parent, local) }
+        if (parent) {
+          if (
+            compareVectors(previousParent, parent) ||
+            compareVectors(previousLocal, local)
+          ) {
+            copy(previousParent, parent)
+            copy(previousLocal, local)
+            mat4.multiply(matrix, parent, local)
+          }
+        } else {
+          mat4.identity(matrix)
+        }
+
         // apply external transform from arguments to computed transform
-        if (transform) { mat4.multiply(matrix, transform, matrix) }
+        if (transform) {
+          if (compareVectors(previousTransform, transform)) {
+            copy(previousTransform, transform)
+            mat4.multiply(matrix, transform, matrix)
+          }
+        }
         return matrix
       },
     }))
+
+    function compareVectors(a, b) {
+      if (a.length != b.length) { return true }
+      for (let i = 0; i < a.length; ++i) {
+        if (a[i] != b[i]) { return true }
+      }
+      return false
+    }
+
+    function copy(a, b) {
+      for (let i = 0; i < b.length; ++i) {
+        a[i] = b[i]
+      }
+    }
+
   }
 }
