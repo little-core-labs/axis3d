32b7767f6c8117e5eeb0f7c77fa9cf866c30450b
Joseph Werle <joseph.werle@gmail.com>
2017-10-06 15:00:49 -0400

    Release 0.6.1

--- dist/axis3d.js
+++ dist/axis3d.js
@@ -7524,7 +7524,7 @@ function _toConsumableArray(arr) {
   }
 }
 
-var kLibraryVersion = '0.6.0';
+var kLibraryVersion = '0.6.1';
 var TypedArray = Object.getPrototypeOf(Float32Array.prototype).constructor;
 
 var HTMLImageElement = _window2.default.HTMLImageElement;
