8ab6043246f3afdce505a139df7afcd9fce3cf33
Joseph Werle <joseph.werle@gmail.com>
2017-10-07 15:42:54 -0400

    refactor(src/core/dynamic.js): Refactor and add esdoc

--- src/core/dynamic.js
+++ src/core/dynamic.js
@@ -1,38 +1,46 @@
 import coalesce from 'defined'
 
+/**
+ * The DynamicValue class represents an object accessor values
+ * that can be dynamically accessed while also providing initial,
+ * or default state. Function values are evaluated with a regl context
+ * object and an arguments object with default state merged in.
+ * @public
+ * @class DynamicValue
+ */
 export class DynamicValue {
+
+  /**
+   * Creates a new DynamicValueCounter
+   * @public
+   * @static
+   * @method
+   * @return {DynamicValueCounter}
+   */
   static createCounter() {
     return new DynamicValueCounter()
   }
 
-  static pluck(scope, property, key, ...defaultValues) {
-    let value = undefined
-    if (scope && null != scope[property]) {
-      value = null != key ? scope[property][key] : scope[property]
-    }
-    return DynamicValue.primitive(coalesce(value, ...defaultValues))
-  }
-
-  static primitive(v) {
-    let value = v
-    if (null == v) { value = undefined }
-    if ('object' == typeof v) {
-      const hasLength = 'number' == typeof v.length
-      const hasIterator = 'function' == typeof v[Symbol.iterator]
-      // array
-      if (hasLength) {
-        if (hasIterator) { value = [...v] }
-        else { value = Array(v.length).fill(0).map((_, i) => v[i]) }
-      }
-      // object
-      else {
-        value = Object.assign({}, v)
-      }
-    }
-    return value
-  }
-
+  /**
+   * DynamicValue class constructor.
+   * @public
+   * @constructor
+   * @param {Context} ctx Axis3D context object
+   * @param {?(Object)} [initialState = {}] Initial state object
+   * @param {?(Object)} [props = {}] Optional initial properties
+   * @throws TypeError
+   */
   constructor(ctx, initialState = {}, props = {}) {
+    if (!ctx || 'object' != typeof ctx || Array.isArray(ctx)) {
+      throw new TypeError("DynamicValue(): Expecting context object.")
+    }
+    if (initialState && 'object' != typeof initialState || Array.isArray(initialState)) {
+      throw new TypeError("DynamicValue(): Expecting initial state to be an object.")
+    }
+
+    if (props && 'object' != typeof props || Array.isArray(props)) {
+      throw new TypeError("DynamicValue(): Expecting props to be an object.")
+    }
     const define = (k, v, def) => Object.defineProperty(this, k, {
       enumerable: false, get: () => v || def
     })
@@ -42,69 +50,50 @@ export class DynamicValue {
     this.set(props)
   }
 
-  set(name, value) {
-    if (name && 'object' == typeof name) {
-      const descriptors = Object.getOwnPropertyDescriptors(name)
-      for (const key in descriptors) {
-        if (null == name[key]) { continue }
-        this.valueState[key] = name[key]
-        try {
-          Object.defineProperty(this, key, {
-            enumerable: true,
-            get: () => this.valueState[key] || this.initialState[key] || null,
-            set:  (v) => this.valueState[key] = v
-          })
-        } catch (e) { }
+  /**
+   * Sets named value by key on instance or an object of
+   * key value pairs.
+   * @public
+   * @method
+   * @param {String|Object} key Value name or object
+   * @param {?(Mixed)} [value] Any value
+   * @return {DynamicValue}
+   * @throws TypeError
+   */
+  set(key, value) {
+    if (key && 'object' == typeof key && !Array.isArray(key) && undefined === value) {
+      for (const k of Object.getOwnPropertyNames(key)) {
+        this.set(k, key[k])
       }
-    } else if ('string' == typeof name && null != value) {
-      this.valueState[name] = value
-      try {
-        Object.defineProperty(this, name, {
-          enumerable: true,
-          set:  (v) => this.valueState[name] = v,
-          get: () => {
-            if ('function' == typeof value) {
-              return (ctx, args) => {
-                if (null == args) { args = {} }
-                if ('object '== typeof args) {
-                  return value(ctx, extend(true, {}, initialState, args))
-                } else {
-                  return value(ctx, args)
-                }
-              }
-            } else {
-              return DynamicValue.primitive(
-                this.valueState[name] ||
-                this.initialState[name]
-              ) || null
-            }
-          },
-        })
-      } catch(e) {}
-    }
-    this.purge()
-    return this
-  }
-
-  unset(name) {
-    if ('string' == typeof name) {
-      delete this[name]
+    } else if ('string' == typeof key || 'number' == typeof key) {
+      if (undefined === value) {
+        throw TypeError("DynamicValue(): Attempting to set undefined value.")
+      } else {
+        createAccessor(this, key)
+        this[key] = value
+      }
+    } else {
+      throw TypeError("DynamicValue(): Attempting to set invalid key value.")
     }
     return this
   }
 
-  purge() {
-    for (const key in this) {
-      if (null == this[key]) {
-        try { delete this[key] }
-        catch (err) { }
-      }
+  /**
+   * Unset value by key name.
+   * @public
+   * @method
+   * @param {String} key Value key name
+   * @return {DynamicValue}
+   */
+  unset(key) {
+    if ('string' == typeof key && key in this.valueState) {
+      delete this.valueState[key]
     }
     return this
   }
 }
 
-export class DynamicValueCounter {
+class DynamicValueCounter {
   constructor() {
     Object.defineProperties(this, {
       contexts: { enumerable: false, value: new Set() },
@@ -134,15 +123,45 @@ export class DynamicValueCounter {
     return this
   }
 
-  sumSetForContext(ctx) {
-    return this.getSetForContext(ctx).size()
-  }
-
   listSetForContext(ctx) {
     return [ ...this.getSetForContext(ctx) ]
   }
 
   list() {
     return [ ...this.contexts ]
+  }
+}
+
+function getPrimitiveValue(v) {
+  let value = v
+  if (null == v) { value = undefined }
+  if (v && 'object' == typeof v) {
+    const hasLength = 'number' == typeof v.length
+    const hasIterator = 'function' == typeof v[Symbol.iterator]
+    // array
+    if (hasLength && hasIterator) {
+      value = [...v]
+    } else if (hasLength) {
+      value = Array(v.length).fill(0).map((_, i) => v[i])
+    } else {
+      value = Object.assign({}, v)
+    }
+  }
+  return value
+}
+
+function createAccessor(object, key) {
+  if (!(key in object)) {
+    return Object.defineProperty(object, key, {enumerable: true, set, get })
+  }
+
+  function set(value) {
+    object.valueState[key] = value
+  }
+
+  function get() {
+    const {valueState, initialState} = object
+    const value = coalesce(valueState[key], initialState[key], null)
+    return getPrimitiveValue(value)
   }
 }
