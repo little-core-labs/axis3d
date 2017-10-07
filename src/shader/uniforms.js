2191f53b20276c83cfb9de46aeb83524f29482cd
Joseph Werle <joseph.werle@gmail.com>
2017-10-06 21:00:54 -0400

    refactor(src/shader): Cleanup shader uniforms and attributes

--- src/shader/uniforms.js
+++ src/shader/uniforms.js
@@ -13,10 +13,5 @@ export function ShaderUniforms(ctx, initialState, props) {
   if ('object' != typeof initialState) { initialState = {} }
   if ('object' != typeof props) { props = initialState }
   const uniforms = new WebGLShaderUniforms(ctx, initialState, props)
-  return ctx.regl({
-    uniforms,
-    context: {
-      uniforms: ({uniforms: prev}) => Object.assign({}, prev, uniforms)
-    }
-  })
+  return ctx.regl({ uniforms, context: { uniforms } })
 }
