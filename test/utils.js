4e8988a65c81748d80bd31c15bd79b6d61605d1b
Joseph Werle <joseph.werle@gmail.com>
2017-10-09 19:47:02 -0400

    test(test/utils.js): Add compare function test helpers

--- test/utils.js
+++ test/utils.js
@@ -1,8 +1,30 @@
-'use strict'
-
+import compareFloat from 'float-equal'
 import { Context } from '../lib/core/context'
+import quat from 'gl-quat'
 
 export const sharedContext = new Context()
 export const xtest = (name, test) => {
-  console.warn('\tskipping test %s', name)
+  setTimeout(() => console.warn('\tSkipping test %s', name))
+}
+
+export const isIdentityQuaternion = (q) => {
+  return false === hasDiff(q, quat.identity([]))
+}
+
+export const hasDiff = (a, b) => {
+  if (Array.isArray(a) && Array.isArray(b)) {
+    return ! a.every((x, i) => compareValue(x, b[i]))
+  } else if (a && 'object' == typeof a && b && 'object' == typeof b) {
+    return ! Object.keys(a).every((k) => compareValue(a[k], b[k]))
+  } else {
+    return ! compareValue(a, b)
+  }
+}
+
+function compareValue(a, b) {
+  if ('number' == typeof a && 'number' == typeof b) {
+    return compareFloat(a, b)
+  } else {
+    return a == b
+  }
 }
